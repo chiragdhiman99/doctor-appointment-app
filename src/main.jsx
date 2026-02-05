e56bc4da0f30ba7d87afb2e5d5a39c40df2dd5d5
Chirag Dhiman <dhimanchirag99@gmail.com>
2026-02-05 12:38:25 +0530

    Fixed routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, HashRouter } from 'react-router'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 createRoot(document.getElementById('root')).render(
       <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-</BrowserRouter>
+</HashRouter>
   </StrictMode>
 )
 
